0540aa4567e8cf824ad479ec5d447ee4ebba7d44
Angelo Gladding <self@angelogladding.com>
2020-10-23 20:09:02 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
-# [`term`][1]: tools for a metamodern terminal environment
-# Copyright (c) MMXVIII- @[Angelo Gladding][2]
+# [`term`][1] Copyright (c) @[Angelo Gladding][2] 2020-
 #
 # This program is free software: it is distributed in the hope that it
 # will be useful, but *without any warranty*; without even the implied
 # warranty of merchantability or fitness for a particular purpose. You
 # can redistribute it and/or modify it under the terms of the @@[GNU's
-# Not Unix][3] [Affero General Public License][4] as published by the
+# Not Unix][3] %[Affero General Public License][4] as published by the
 # @@[Free Software Foundation][5], either version 3 of the License, or
 # any later version.
 #
 # *[GNU]: GNU's Not Unix
 #
-# [1]: //lahacker.net/code/term
-# [2]: //lahacker.net
-# [3]: //gnu.org
-# [4]: //gnu.org/licenses/agpl
-# [5]: //fsf.org
+# [1]: https://github.com/angelogladding/term
+# [2]: https://angelogladding.com
+# [3]: https://gnu.org
+# [4]: https://gnu.org/licenses/agpl
+# [5]: https://fsf.org
+
+"""Tools for metamodern terminal development."""
 
 from setuptools import setup
 
